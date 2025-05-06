990b9d6c8c0c14928ef00f57fa102eae9dc1bf61
HermanKassler <tore.gaani@gmail.com>
2025-05-06 14:38:53 +0200

    change rebinds to be links

--- .config/nvim/lua/custom/plugins/colorscheme.lua
+++ .config/nvim/lua/custom/plugins/colorscheme.lua
@@ -6,13 +6,12 @@ return {
       -- Link to info regarding noice highlight groups:
       -- https://github.com/folke/noice.nvim/blob/bf67d70bd7265d075191e7812d8eb42b9791f737/doc/noice.nvim.txt#l799
       local colors = require('gruvbox').palette
-      vim.api.nvim_set_hl(0, 'NoiceCmdlinePopupBorder', { fg = colors.bright_aqua, bg = colors.dark0 })
-      vim.api.nvim_set_hl(0, 'NoiceCmdlineicon', { fg = colors.bright_aqua, bg = colors.dark0 })
-      vim.api.nvim_set_hl(0, 'NoiceCmdlinePopupBorderSearch', { fg = colors.bright_yellow, bg = colors.dark0 })
-      vim.api.nvim_set_hl(0, 'NoiceCmdlineiconSearch', { fg = colors.neutral_yellow, bg = colors.dark0 })
+      vim.api.nvim_set_hl(0, 'NoiceCmdlinePopupBorder', { link = 'GruvboxBg0' })
+      vim.api.nvim_set_hl(0, 'NoiceCmdlineicon', { link = 'GruvboxBg0' })
+      -- vim.api.nvim_set_hl(0, 'NoiceCmdlinePopupBorderSearch', { link = 'GruvboxYellow' })
+      -- vim.api.nvim_set_hl(0, 'NoiceCmdlineiconSearch', { link = 'GruvboxYellow' })
       --harpoon
-      vim.api.nvim_set_hl(0, 'HarpoonBorder', { fg = colors.dark_red, bg = colors.dark_red })
-      vim.api.nvim_set_hl(0, 'HarpoonWindow', { fg = colors.dark0, bg = colors.dark0 })
+      vim.api.nvim_set_hl(0, 'HarpoonWindow', { link = 'GruvboxBg0' })
       --snacks
       vim.api.nvim_set_hl(0, 'SnacksIndentScope', { link = 'GruvboxGray' })
 
@@ -22,11 +21,13 @@ return {
         overrides = {
           -- fixes harpoon color issues
           NormalFloat = { fg = colors.light0, bg = colors.dark0 },
+          Normal = { fg = colors.light0, bg = colors.dark0 },
           FloatBorder = { fg = colors.light0, bg = colors.dark0 },
           SignColumn = { bg = colors.dark0 },
           CursorLineNr = { bg = colors.dark0 },
           FoldColumn = { bg = colors.dark0 },
           GruvboxYellowSign = { bg = colors.dark0 },
+          GruvboxRedSign = { bg = colors.dark0 },
         },
       }
       vim.cmd.colorscheme 'gruvbox'
